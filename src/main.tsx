56877074765aaba2d56373a98c631051b38ec144
krdlange <krdlange@gmail.com>
2023-06-12 23:54:17 +0200

    installed and configured tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// entry point for entire app
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
